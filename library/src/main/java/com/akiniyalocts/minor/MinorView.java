0bdc63340fb5fe8618962260d7d9b72e5e044e24
akiniyalocts <kiniyaloca1@gmail.com>
2016-05-17 11:25:58 -0400

    notification badge support

--- library/src/main/java/com/akiniyalocts/minor/MinorView.java
+++ library/src/main/java/com/akiniyalocts/minor/MinorView.java
@@ -9,13 +9,14 @@ import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
  * Created by anthonykiniyalocts on 5/13/16.
  */
-public class MinorView extends LinearLayout{
+public class MinorView extends FrameLayout{
 
     public MinorView(Context context) {
         super(context);
@@ -59,12 +60,8 @@ public class MinorView extends LinearLayout{
                 0, 0
         );
 
-        params = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 
-        this.setLayoutParams(getLayoutParamsForIconView());
-        this.setOrientation(VERTICAL);
-
-
+        // get attributes
         int iconViewRes = a.getResourceId(R.styleable.MinorView_minor_icon_view, -1);
 
         String title = a.getString(R.styleable.MinorView_minor_title);
@@ -74,9 +71,27 @@ public class MinorView extends LinearLayout{
         selectedTitleColor = a.getColor(R.styleable.MinorView_minor_title_selected_color, -1);
 
 
+
+        // Build layout
+        LinearLayout minorLayout = new LinearLayout(getContext());
+
+        minorLayout.setOrientation(LinearLayout.VERTICAL);
+
+
+        minorLayout.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1f));
+
+        minorLayout.setGravity(Gravity.CENTER_HORIZONTAL);
+
+        minorLayout.setPadding(5, 5, 5, 5);
+
+
+        this.setClickable(true);
+
+
+        // Add icon and title
         if(iconViewRes != -1){
             View iconView = inflate(context, iconViewRes, null);
-            this.addView(iconView, getLayoutParamsForIconView());
+            minorLayout.addView(iconView, getLayoutParamsForIconView());
         }
         else {
             throw new IllegalArgumentException("You must specify a view for MinorView to inflate as an icon. Use app:minor_icon_view=@layout/your_view");
@@ -87,6 +102,7 @@ public class MinorView extends LinearLayout{
 
             titleTextView.setLayoutParams(getLayoutParamsForIconView());
             titleTextView.setText(title);
+            titleTextView.setClickable(false);
 
             titleTextView.setTextColor(titleColor);
 
@@ -96,25 +112,24 @@ public class MinorView extends LinearLayout{
                 }
             }
 
-            this.addView(titleTextView);
+            titleTextView.setGravity(Gravity.CENTER_HORIZONTAL);
+            minorLayout.addView(titleTextView);
         }
 
 
 
-        this.setGravity(Gravity.CENTER_HORIZONTAL);
+        minorLayout.requestLayout();
 
-        this.setPadding(10, 10, 10, 10);
-
-        this.requestLayout();
-
+        this.addView(minorLayout);
 
         a.recycle();
 
     }
 
+
     private LayoutParams getLayoutParamsForIconView(){
         if(params == null) {
-            params = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f);
+            params = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         }
         return params;
     }
@@ -127,6 +142,22 @@ public class MinorView extends LinearLayout{
                 invalidate();
             }
         }
+    }
+
+    public void addNotifcation(int notificationCount){
+        FrameLayout notificationView = (FrameLayout) inflate(getContext(), R.layout.minor_notification, null);
+
+        TextView notificationTextView = (TextView) notificationView.findViewById(R.id.minor_notification_text);
+
+
+        notificationTextView.setText(String.valueOf(notificationCount));
+        LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.TOP | Gravity.RIGHT);
+        params.setMargins(20,15,5,5);
+        notificationView.setLayoutParams(params);
+
+        this.addView(notificationView);
+
+        invalidate();
     }
 
     public void unselected(){
--- minorsample/src/main/java/com/akiniyalocts/minorsample/MainActivity.java
+++ minorsample/src/main/java/com/akiniyalocts/minorsample/MainActivity.java
@@ -1,7 +1,7 @@
 package com.akiniyalocts.minorsample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.akiniyalocts.minor.MinorView;
@@ -13,9 +13,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MinorView home = (MinorView)findViewById(R.id.last_home);
+        final MinorView home = (MinorView)findViewById(R.id.last_home);
 
         home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                home.selected();
+            }
+        });
+
+        home.addNotifcation(8);
+
+        final MinorView lists = (MinorView)findViewById(R.id.lists);
+
+        lists.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
